046aa1c7c55f81453ba1950625286ff3395f7343
liweijun <liweijun0302@gmail.com>
2024-09-25 10:46:24 +0800

    Site updated: 2024-09-25 10:46:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/25/Park/","2024/09/25/Investment/","2024/09/25/Tourism/"];function toRandomPost(){
+var posts=["2024/09/25/Park/","2024/09/25/CulturalPlace/","2024/09/25/GoodServices/","2024/09/25/ImportedProducts/","2024/09/25/InvestmentArt/","2024/09/25/English/","2024/09/25/Investment/","2024/09/25/Tourism/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
