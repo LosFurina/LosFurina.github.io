b8d1eccb817668f9b8c2c95b9ad0a29b0124892a
liweijun <liweijun0302@gmail.com>
2024-09-25 08:21:20 +0800

    Site updated: 2024-09-25 08:21:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/25/Tourism/","2024/09/25/Garden/","2024/09/24/af-githubio-hexo-deploy/","2024/09/24/hello-world/"];function toRandomPost(){
+var posts=["2024/09/25/Park/","2024/09/25/Tourism/","2024/09/24/hello-world/","2024/09/24/af-githubio-hexo-deploy/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
